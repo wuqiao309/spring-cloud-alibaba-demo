71fc954e12dfe13350f0db298b1e5d22a3f4b33d
Administrator <wuqiao309@163.com>
2022-08-31 00:26:43 +0800

    一些配置

--- dubbo-consumer-demo/src/main/java/consumerdemo/service/TestService.java
+++ dubbo-consumer-demo/src/main/java/consumerdemo/service/TestService.java
@@ -1,5 +1,7 @@
 package consumerdemo.service;
 
+import org.apache.dubbo.common.constants.ClusterRules;
+import org.apache.dubbo.common.constants.LoadbalanceRules;
 import org.apache.dubbo.config.annotation.DubboReference;
 import org.springframework.stereotype.Service;
 
@@ -11,7 +13,14 @@ import dubbo.sdk.DubboServiceDemo;
  */
 @Service
 public class TestService {
-    @DubboReference
+    @DubboReference(
+            loadbalance = LoadbalanceRules.RANDOM, // 负载均衡
+            cluster = ClusterRules.FAIL_OVER, // 服务容错
+            retries = 2, // 失败重试次数
+            timeout = 1000, // 超时时间。
+            mock = "return ok", // 降级、mock
+            async = false // 异步调用
+    )
     private DubboServiceDemo dubboServiceDemo;
 
     public String showMsg(String str) {
