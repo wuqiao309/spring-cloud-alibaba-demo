6ec5012532cbe48d350280adec5cc3e4cfcb69fd
Administrator <wuqiao309@163.com>
2022-09-17 22:02:39 +0800

    描述

--- nacos-consumer-demo/src/main/java/nacosconfig/feign/FeignConfig.java
+++ nacos-consumer-demo/src/main/java/nacosconfig/feign/FeignConfig.java
@@ -11,6 +11,7 @@ import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandl
 import feign.Feign;
 
 /**
+ * 解决@RequestMapping、@FeignClient 同时出现，且有路径冲突，启动有requestMapping冲突错误
  * @author wuqiao
  * Created on 2022-09-17
  */
